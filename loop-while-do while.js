b0de5961ad4e8a6615c7843dad79d3d29fbee152
ASHMIRC <126319208+ASHMIRC@users.noreply.github.com>
2024-03-25 22:07:13 +0530

    Create loop-while-do while.js

---
+++ loop-while-do while.js
@@ -0,0 +1,27 @@
+//while loop
+
+let i=1
+while (i<=5)
+{
+    console.log('hello')
+    i++
+}
+
+
+//do while loop
+
+let i=1
+do
+{
+    console.log('hello')
+    i++
+} while (i<=5)*/
+
+//for loop
+
+for (let i=1;i<=5;i++)
+{
+    console.log('hello')
+    for (let j=1;j<=5;j++)
+        console.log('hi')
+}
